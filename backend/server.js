6e8c003c43a076bea3a3fd7d93bcb721c5a704a8
SivuyileMtwetwe <sivuyilemtwetwe@gmail.com>
2025-03-11 10:24:20 +0200

    vercel file

--- backend/server.js
+++ backend/server.js
@@ -5,3 +5,8 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+
+app.get('/',  async(_req, res) => {
+  res.status(200).send("Welcome to the eternal rest api")
+})
